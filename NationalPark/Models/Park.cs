bb319920dfeb0b2dd58cd9494678363e2c7e4b25
Sean Downs <spdowns@protonmail.com>
2020-08-21 10:51:46 -0700

    finish the API model by adding a reqired to name and location. Going on to swagger and versioning

--- NationalPark/Models/Park.cs
+++ NationalPark/Models/Park.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace NationalPark.Models
 {
   public class Park
   {
     public int ParkId { get; set; }
+    [Required]
     public string Name { get; set; }
+    [Required]
     public string Location { get; set; }
     public string Description { get; set; }
     public string DateEstablished  { get; set; }
