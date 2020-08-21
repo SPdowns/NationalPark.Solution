4d838e213a412fb97ae85a463a98eb85d6dfc534
Sean Downs <spdowns@protonmail.com>
2020-08-21 08:50:09 -0700

    comment out secure redirect, also going through weeks lessons to follow workflow path

--- NationalPark/Startup.cs
+++ NationalPark/Startup.cs
@@ -41,7 +41,7 @@ namespace NationalPark
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
